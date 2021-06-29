fc78f5497bbea10f2fa4db05e931f037f3a71fd5
Taisuke Fukuno <fukuno@jig.jp>
2021-06-29 22:24:26 +0900

    add ES module

---
+++ es/XLSX.js
@@ -0,0 +1,23 @@
+import { XLSX as xlsx } from "./xlsx.full.min.js";
+import { xlsx2csv } from "./xlsx2csv.js";
+
+const decode = (bin) => {
+  const ws = xlsx.read(bin, { type: "array" });
+  return ws;
+};
+const toCSV = (wsorsheet) => {
+  if (wsorsheet.Sheets) {
+    const ws = wsorsheet;
+    const sh = ws.Sheets[ws.SheetNames[0]];
+    return xlsx2csv(sh);
+  } else {
+    const sh = wsorsheet;
+    return xlsx2csv(sh);
+  }
+};
+
+const XLSX = {
+  decode,
+  toCSV,
+};
+export { XLSX };
---
+++ es/test/es.test.js
@@ -0,0 +1,14 @@
+import { XLSX } from "../XLSX.js";
+import { CSV } from "https://js.sabae.cc/CSV.js";
+
+const fn = "test.xlsx";
+
+const bin = new Uint8Array(await Deno.readFile(fn));
+const ws = XLSX.decode(bin);
+console.log(ws);
+
+const csv = XLSX.toCSV(ws);
+console.log(csv);
+await Deno.writeTextFile("test.csv", CSV.encode(csv));
+
+
